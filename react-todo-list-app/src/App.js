c3fc9f27ba1c0eff409565a7a8ac9f18b615c5e4
Kaori Kawakami <k.kawakami08@gmail.com>
2022-09-22 17:15:56 -0700

    basic layout

--- react-todo-list-app/src/App.js
+++ react-todo-list-app/src/App.js
@@ -2,7 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="container">
+      <h1>Kaori's Todo List</h1>
+      <div className="todo-input">
+        <form>
+          <input type="text" placeholder="What do I have to do today?" />
+          <button className="add-task-btn">Add Task</button>
+          <select>
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="incomplete">Incomplete</option>
+          </select>
+        </form>
+      </div>
+      <div className="todo-list">
+        <ul>
+          <li>
+            <div className="todo-list">
+              <p>First Todo</p>
+              <div className="box-container">
+                <div className="todo-box check">✓</div>
+                <div className="todo-box delete">✕</div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
